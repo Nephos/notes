ff376fd6a0ed4537da2fc2b24ae6cf7f4c4003ca
Arthur Poulet <arthur.poulet@mailoo.org>
2017-03-31 14:23:03 +0100

    Add title modification feature

--- src/notes.cr
+++ src/notes.cr
@@ -13,9 +13,11 @@ file_path = File.expand_path("#{date}.md", notes_path)
 note = ""
 action = :add
 increment = true
+title = time
 
 parsed = OptionParser.parse! do |p|
   p.banner = "Usage: notes [arguments]"
+  p.on("-t=TITLE", "--title=TITLE", "Change de title (default = HOUR:MINUTE)") { |arg_title| title = arg_title }
   p.on("-h", "--help", "Show this help") { puts p; exit }
   p.on("-P", "--show-path", "Show this current file path") { puts file_path; exit }
   p.on("-I", "--no-increment", "Do not increment the last note") { increment = false }
@@ -28,13 +30,13 @@ when :show
   puts File.read(file_path)
 when :add
   Dir.mkdir notes_path rescue nil
-  data = if increment && !File.read(file_path).match(/^# #{time}$/m).nil?
+  data = if increment && !File.read(file_path).match(/^# #{title}$/m).nil?
            "
 #{note}
 
 "
          else
-           "# #{time}
+           "# #{title}
 #{note}
 
 "
